7925dd1d66cf202c9f8ff30136f3f847a3f543df
Jonas Yaich <jonasyaich@MacBook-Air-tilhrende-Jonas.local>
2025-11-13 14:42:29 +0100

    Landing Page Render

---
+++ src/unAuth/landingpage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './landingpage.css';
+
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <h1 className="landing-title">Hello Landing Page</h1>
+    </div>
+  );
+}
+
+export default LandingPage;
+
