e35b047bf1ebfeffca7c505d2f778874d7ce3362
Jonas Yaich <jonasyaich@MacBook-Air-tilhrende-Jonas.local>
2025-11-22 10:12:35 +0100

    landingpage opdateret

--- src/unAuth/components/frontpage.js
+++ src/unAuth/components/frontpage.js
@@ -1,30 +1,100 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { SplineSceneBasic } from 'components/ui/spline-demo';
-import './frontpage.css';
+import React, { useEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import "./frontpage.css";
 
-function Frontpage() {
+const clips = [
+    "/Hero/4110584-uhd_3840_2160_30fps.mp4",
+    "/Hero/4327192-uhd_4096_2160_25fps.mp4",
+    "/Hero/4489829-uhd_3840_2160_25fps.mp4",
+    "/Hero/5793441-uhd_3840_2160_25fps.mp4",
+    "/Hero/5793444-uhd_3840_2160_25fps.mp4",
+    "/Hero/6111110-uhd_3840_2160_25fps.mp4",
+    "/Hero/11492178-uhd_3840_2160_50fps.mp4",
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          ];
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          function Frontpage() {
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            const [active, setActive] = useState(0);
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            const [next, setNext] = useState(1);
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            const [isFading, setIsFading] = useState(false);
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            const activeRef = useRef(null);
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            const nextRef = useRef(null);
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            useEffect(() => {
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              const n = (active + 1) % clips.length;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              setNext(n);
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              if (nextRef.current) {
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                nextRef.current.src = clips[n];
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                nextRef.current.load();
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              }
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              if (activeRef.current?.paused) {
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                activeRef.current.play().catch(() => {});
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            }, [active]);
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            const handleEnded = () => {
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              setIsFading(true);
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              nextRef.current?.play().catch(() => {});
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              setTimeout(() => {
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                setActive(next);
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                setIsFading(false);
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              }, 900);
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            };
+
   return (
     <section className="frontpage">
+      <div className="frontpage-video-bg" aria-hidden="true">
+        <video
+          ref={activeRef}
+          className={`frontpage-video ${isFading ? "fade-out" : "fade-in"}`}
+          src={clips[active]}
+          autoPlay
+          muted
+          playsInline
+          preload="auto"
+          onEnded={handleEnded}
+        />
+
+        <video
+          ref={nextRef}
+          className={`frontpage-video ${isFading ? "fade-in" : "fade-out"}`}
+          src={clips[next]}
+          muted
+          playsInline
+          preload="auto"
+        />
+
+        <div className="frontpage-video-overlay" />
+      </div>
+
       <div className="frontpage-grid">
         <div className="frontpage-container">
           <h1 className="frontpage-title">
             <span className="frontpage-title-line">Stay focused on your patients.</span>
-            <span className="frontpage-title-line">We'll handle the rest<span className="frontpage-dot">.</span></span>
+            <span className="frontpage-title-line">
+              We'll handle the rest<span className="frontpage-dot">.</span>
+            </span>
           </h1>
-          <p className="frontpage-subtitle">Det eneste redskab, du behøver for at klare det hele.</p>
-          <p className="frontpage-description">
-            Din AI-assistent transskriberer journaler, hjælper med cases, tager opkald og styrer booking – så du kan være den fysioterapeut du blev uddannet til og udvikle dig til en bedre.
+
+          <p className="frontpage-subtitle">
+            Det eneste redskab, du behøver for at klare det hele.
           </p>
+
           <div className="frontpage-buttons">
-            <Link to="/signup" className="frontpage-button primary">Start for free</Link>
-            <a href="#demo" className="frontpage-button secondary">Book a demo</a>
+            <Link to="/signup" className="frontpage-button primary">
+              Start for free
+            </Link>
+            <a href="#demo" className="frontpage-button secondary">
+              Book a demo
+            </a>
           </div>
         </div>
-        <div className="frontpage-showcase" id="demo">
-          <SplineSceneBasic />
-        </div>
       </div>
+      <div id="demo" className="frontpage-anchor" aria-hidden="true" />
     </section>
   );
 }
--- src/unAuth/landingpage.js
+++ src/unAuth/landingpage.js
@@ -2,30 +2,21 @@ import React from 'react';
 import './landingpage.css';
 import Navbar from './components/navbar';
 import Frontpage from './components/frontpage';
-import { KalenderScrollDemo } from '../components/ui/kalender-scroll';
+import MacbookScrollDemo from '../components/ui/macbook-scroll-demo';
 import Both from './components/Both';
 import Footer from './components/footer';
-
-const LandingDivider = () => (
-  <div className="landing-divider" aria-label="Divider: from physios to physios">
-    <span className="landing-divider-text">from physios to physios</span>
-  </div>
-);
 
 function LandingPage() {
   return (
     <div className="landing-page">
       <Navbar />
-      <LandingDivider />
       <main className="landing-page-main">
         <section className="landing-section landing-section-hero" id="hero">
           <Frontpage />
         </section>
-        <LandingDivider />
         <section className="landing-section landing-section-panel" id="kalender">
-          <KalenderScrollDemo />
+          <MacbookScrollDemo />
         </section>
-        <LandingDivider />
         <section className="landing-section landing-section-full" id="suite">
           <Both />
         </section>
